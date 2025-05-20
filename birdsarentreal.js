11dfb8ecfb2f4f0dff9573cf66318fff49c11220
MineErich <public.fp@posteo.de>
2025-05-20 21:10:21 +0200

    ende des spiels: voegel und spys koenne nicht mehr abgeschossen werden

--- birdsarentreal.js
+++ birdsarentreal.js
@@ -1,22 +1,38 @@
-// init score
-let score = 0;
+    // init score
+    let score;
 
-// scoreboard
-let html_score = document.getElementById("shop-name");
-html_score.innerHTML = "<h1>score: " + score + "</h1>";
+    // scoreboard
+    let html_score;
 
-// store initial settings
-let orig_cursor = document.getElementById("splash").style.cursor;
-let orig_pointerEvents = document.getElementsByClassName("global-header")[0].style.pointerEvents;
+    // store initial settings
+    let orig_cursor;
+    let orig_pointerEvents;
+    let orig_ani;
+    let orig_ani_spy;
 
-// add play button
-let menu = document.getElementsByClassName("header-links")[0];
-let play_btn = document.createElement("button");
-play_btn.innerHTML = "&#9658";
-play_btn.classList.add("header-link", "btn-primary", "btn-sm");
-play_btn.id = "play_btn";
-play_btn.addEventListener("click", start_game());
-menu.insertBefore(play_btn, menu.childNodes[0]);
+// document.addEventListener("DOMContentLoaded", (event) => {
+    // init score
+    score = 0;
+
+    // scoreboard
+    html_score = document.getElementById("shop-name");
+    html_score.innerHTML = "<h1>score: " + score + "</h1>";
+
+    // store initial settings
+    orig_cursor = document.getElementById("splash").style.cursor;
+    orig_pointerEvents = document.getElementsByClassName("global-header")[0].style.pointerEvents;
+    orig_ani = document.getElementById("goose").style.animation;
+    orig_ani_spy = document.getElementById("plane-video").children[0].children[0].style.animation;
+
+    // add play button
+    let menu = document.getElementsByClassName("header-links")[0];
+    let play_btn = document.createElement("button");
+    play_btn.innerHTML = "&#9658";
+    play_btn.classList.add("header-link", "btn-primary", "btn-sm");
+    play_btn.id = "play_btn";
+    play_btn.addEventListener("click", (e) => init_game(e));
+    menu.insertBefore(play_btn, menu.childNodes[0]);
+// });
 
 // add timer (60s)
 function startTimer(count, display) {
@@ -32,8 +48,13 @@ function startTimer(count, display) {
     }, 1000);
 }
 
-function click_bird(id) {
-    item = document.getElementById(id);
+function init_game(event) {
+    start_game();
+    event.stopPropagating;
+}
+
+function click_bird(e, item) {
+    // let item = document.getElementById(id);
     item.style.top = `${Math.random() * 70 + 30}vh`;
     item.style.left = "100vw";
     const ani = item.style.animation;
@@ -45,10 +66,11 @@ function click_bird(id) {
 
     score += 10;
     html_score.innerHTML = "<h1>score: " + score + "</h1>";
+
+    e.stopPropagating;
 }
 
-function click_spy(id) {
-    item = document.getElementById(id);
+function click_spy(e, item) {
     item.style.left = "100vw";
     const ani = item.style.animation;
     item.style.animation = undefined;
@@ -59,6 +81,8 @@ function click_spy(id) {
 
     score -= 50;
     html_score.innerHTML = "<h1>score: " + score + "</h1>";
+
+    e.stopPropagating;
 }
 
 // the game
@@ -66,7 +90,7 @@ function start_game() {
 
     html_score.innerHTML = "<h1>score: " + score + "</h1>";
 
-    startTimer(60, document.getElementById("play_btn"));
+    startTimer(10, document.getElementById("play_btn"));
 
     document.getElementById("splash").style.cursor = "crosshair";
     document.getElementsByClassName("global-header")[0].style.pointerEvents = "none";
@@ -75,24 +99,23 @@ function start_game() {
     let birds = document.getElementsByClassName("birds")[0];
     var bird = birds.childNodes;
     bird.forEach(function (item) {
-        console.log(item.tagName)
-        if (item.tagName == "img" || item.tagName == "IMG") {
-            item.addEventListener("click", click_bird(item.id));
+        if (item.tagName == "IMG") {
+            item.addEventListener("click", (e) => click_bird(e, item));
         }
     });
 
     // make other objects clickable
-    // spying(document.getElementById("plane-video"));
-    // spying(document.getElementById("parachute"));
-    // spying(document.getElementById("blimp"));
-    // spying(document.getElementsByClassName("road")[0].children[0]);
-    // spying(document.getElementsByClassName("road")[0].children[1]);
+    spying(document.getElementById("plane-video"));
+    spying(document.getElementById("parachute"));
+    spying(document.getElementById("blimp"));
+    spying(document.getElementsByClassName("road")[0].children[0]);
+    spying(document.getElementsByClassName("road")[0].children[1]);
 
 }
 
 function spying(spies) {
     var item = spies;
-    item.addEventListener("click", click_spy(item.id));
+    item.addEventListener("click", (e) => click_spy(e, item));
 
     const spy = spies.children;
     Array.from(spy).forEach(function (item) {
@@ -107,4 +130,7 @@ function clear_game() {
     document.getElementById("play_btn").innerHTML = "&#9658";
     document.getElementById("splash").style.cursor = orig_cursor;
     document.getElementsByClassName("global-header")[0].style.pointerEvents = orig_pointerEvents;
+
+    click_bird = function() {};
+    click_spy = function() {};
 }
